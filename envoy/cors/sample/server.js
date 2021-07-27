d9d193e734c950d53885b26fdd3aaf9788ad6d37
koketani <ykoketani@gmail.com>
2021-07-27 10:44:28 +0900

    envoy; udpate local client/server application to encounter cors

--- envoy/cors/sample/server.js
+++ envoy/cors/sample/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
-app.use(
-  cors({
-    origin: "http://localhost:8000",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:8000",
+//   })
+// );
 
 app.get("/hello", (_, res) => {
   res.json({ name: "alice" });
