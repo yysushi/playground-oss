fd81e26a50f76aaf5c72802bfb7ef3934a7dce47
yysushi <yoshihiro.tsuji2@gmail.com>
2023-12-15 21:33:57 +0900

    update pubsub sample

--- pubsub/main_test.go
+++ pubsub/main_test.go
@@ -2,75 +2,194 @@ package main_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"golang.org/x/sync/errgroup"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/api/iterator"
+	"google.golang.org/grpc/status"
 )
 
-func provision(ctx context.Context) error {
-	client, err := pubsub.NewClient(ctx, "testproject")
-	if err != nil {
-		return err
-	}
-	defer client.Close()
-	fmt.Println("connected by provisioner")
-	_, err = client.CreateTopic(ctx, "testtopic")
-	if err != nil {
-		fmt.Printf("%s in provisioner", err)
-		return err
-	}
-	fmt.Println("created topic")
-	return nil
+var knownErrMsgs = []string{
+	"Topic already exists",
+	"Subscription already exists",
 }
 
-func TestPubSub(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	// provisioner
-	if err := provision(ctx); err != nil {
-		log.Fatal(err)
+func isKnownError(err error) bool {
+	s, ok := status.FromError(err)
+	for _, errMsg := range knownErrMsgs {
+		if ok && s.Message() == errMsg {
+			return true
+		}
 	}
-	eg, ctx := errgroup.WithContext(ctx)
-	// pubslisher
-	eg.Go(func() error {
-		client, err := pubsub.NewClient(ctx, "testproject")
-		if err != nil {
-			return err
-		}
-		defer client.Close()
-		fmt.Println("connected by publisher")
-		it := client.Topics(ctx)
-		for {
-			t, err := it.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				fmt.Printf("%s in publisher", err)
-				return err
-			}
-			fmt.Println(t)
-		}
-		return nil
-	})
-	// subscriber
-	eg.Go(func() error {
-		client, err := pubsub.NewClient(ctx, "testproject")
-		if err != nil {
-			fmt.Printf("%s in subscriber", err)
-			return err
-		}
-		defer client.Close()
-		fmt.Println("connected by subscriber")
-		return nil
-	})
+	return false
+}
 
-	if err := eg.Wait(); err != nil {
+func setUp(t *testing.T, ctx context.Context) (client *pubsub.Client) {
+	var err error
+	client, err = pubsub.NewClient(ctx, "testproject")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var topic *pubsub.Topic
+	topic, err = client.CreateTopic(ctx, "testtopic")
+	if err != nil {
+		if !isKnownError(err) {
+			t.Fatal(err)
+		}
+		topic = client.Topic("testtopic")
+	}
+	_, err = client.CreateSubscription(ctx, "testsubscription", pubsub.SubscriptionConfig{
+		Topic:            topic,
+		AckDeadline:      10 * time.Second,
+		ExpirationPolicy: time.Duration(0),
+	})
+	if err != nil && !isKnownError(err) {
+		t.Fatal(err)
+	}
+	return
+}
+
+func tearDown(t *testing.T, ctx context.Context, client *pubsub.Client) {
+	client.Close()
+}
+
+func TestPubSub_topics(t *testing.T) {
+	ctx := context.Background()
+	client := setUp(t, ctx)
+	defer tearDown(t, ctx, client)
+
+	it := client.Topics(ctx)
+	topics := make([]*pubsub.Topic, 0)
+	for {
+		topic, err := it.Next()
+		if err != nil {
+			if errors.Is(err, iterator.Done) {
+				break
+			} else {
+				t.Fatal(err)
+			}
+		}
+		topics = append(topics, topic)
+	}
+	assert.Len(t, topics, 1)
+	assert.Equal(t, "testtopic", topics[0].ID())
+}
+
+type OrderResult int64
+
+const (
+	InOrder OrderResult = iota
+	UnOrder
+)
+
+// Reception order is not the same as sent
+// (use SubscriptionConfig.EnableMessageOrdering if needed)
+// https://github.com/googleapis/google-cloud-go/blob/pubsub/v1.33.0/pubsub/subscription.go#L533
+func TestPubSub_order(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	client := setUp(t, ctx)
+	defer tearDown(t, ctx, client)
+
+	met := make(map[OrderResult]struct{})
+	var i = 0
+	for ; i < 100; i++ {
+		result := func() OrderResult {
+			topic := client.Topic("testtopic")
+			res := topic.Publish(ctx, &pubsub.Message{Data: []byte("1")})
+			_, err = res.Get(ctx)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res = topic.Publish(ctx, &pubsub.Message{Data: []byte("2")})
+			_, err = res.Get(ctx)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			cctx, cancel := context.WithCancel(ctx)
+			msgs := make([]string, 0)
+			receivedMsgs := make(chan string)
+			done := make(chan error)
+			subscription := client.Subscription("testsubscription")
+			go func() {
+				done <- subscription.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+					receivedMsgs <- string(msg.Data)
+					msg.Ack()
+				})
+			}()
+			msgs = append(msgs, <-receivedMsgs)
+			msgs = append(msgs, <-receivedMsgs)
+			cancel()
+			if err = <-done; err != nil && !errors.Is(err, context.Canceled) {
+				t.Fatal(err)
+			}
+			assert.ElementsMatch(t, []string{"1", "2"}, msgs)
+			if msgs[0] == "1" {
+				return InOrder
+			}
+			return UnOrder
+		}()
+		met[result] = struct{}{}
+		if l := len(met); l == 1 {
+			fmt.Println(i, result)
+		} else if l == 2 {
+			fmt.Println(i, result)
+			return
+		}
+	}
+	t.Fatal("reached to max loop")
+}
+
+// Nack affects only one message
+// (the nacked message is only resent)
+func TestPubSub_nack(t *testing.T) {
+	var err error
+	ctx := context.Background()
+	client := setUp(t, ctx)
+	defer tearDown(t, ctx, client)
+
+	topic := client.Topic("testtopic")
+	res := topic.Publish(ctx, &pubsub.Message{Data: []byte("1")})
+	_, err = res.Get(ctx)
+	if err != nil {
 		log.Fatal(err)
 	}
+	res = topic.Publish(ctx, &pubsub.Message{Data: []byte("2")})
+	_, err = res.Get(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan error)
+	cctx, cancel := context.WithCancel(ctx)
+	receivedMsgs := make(chan *pubsub.Message)
+	ret := make(chan func())
+	subscription := client.Subscription("testsubscription")
+	go func() {
+		done <- subscription.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+			receivedMsgs <- msg
+			f := <-ret
+			f()
+		})
+	}()
+
+	firstMsg := <-receivedMsgs
+	ret <- firstMsg.Nack
+	secondMsg := <-receivedMsgs
+	ret <- secondMsg.Ack
+	thirdMsg := <-receivedMsgs
+	ret <- thirdMsg.Ack
+
+	cancel()
+	if err = <-done; err != nil && !errors.Is(err, context.Canceled) {
+		t.Fatal(err)
+	}
+	assert.Equal(t, firstMsg.Data, thirdMsg.Data)
+	assert.NotEqual(t, firstMsg.Data, secondMsg.Data)
 }
